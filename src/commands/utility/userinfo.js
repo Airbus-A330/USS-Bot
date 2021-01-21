5979fdda99cddcd2e06a89cbe4841266e75b4351
AiyrTech <danielrperkins05@gmail.com>
2021-01-21 15:19:11 +0000

    stuff

--- src/commands/utility/userinfo.js
+++ src/commands/utility/userinfo.js
@@ -1,19 +1,37 @@
+const Discord = require("discord.js");
+
 module.exports = {
     name: 'userinfo',
     description: 'get info on a user',
     execute (client, message, args) {
-        let user = message.mentions.users.first();
-        message.channel.send(user);
-    },
-}
-
-/*
-        let user = message.mentions.users.first();
-        message.channel.send(user);
-        if (!user) {
+        let member = message.mentions.members.first();
+        if (!member) {
             if (!Number.isNaN(args[0])) {
-                user = message.guild.users.cache.get(args[0]);
-                message.channel.send(user);
+                member = message.guild.members.cache.get(`${args[0]}`);
             }
         }
-*/
+        if (!member) {
+            const noUser = new Discord.MessageEmbed()
+                .setColor('RANDOM')
+                .setDescription('Please give a valid mention or userID!')
+            return message.channel.send(noUser)
+        }
+//        let user = userID.id;
+        console.log(member);
+
+        const userInfo = new Discord.MessageEmbed()
+            .setColor('RANDOM')
+            .setTitle(`Userinfo - ${member.user.tag}`)
+            .setThumbnail(`${member.user.displayAvatarURL({ format: 'png', dynami: true })}`)
+            .addFields(
+                { name: 'Tag', value: `${member.user.username}#${member.user.discriminator}`, inline: true },
+                { name: 'Mention', value: `${member}`, inline: true },
+                { name: 'UserID', value: `${member.id}`, inline: true },
+                { name: 'Joined', value: `${member.joinedAt.toLocaleDateString()}, at ${member.joinedAt.toLocaleTimeString()}`, inline: true },
+                { name: 'Created', value: `${member.user.createdAt.toLocaleDateString()}, at ${member.user.createdAt.toLocaleTimeString()}`, inline: true },
+            )
+            .addField('Roles', `${member.roles.cache.map(role => role.toString()).join(',')}`, false)
+            .setFooter(`Requested by ${message.author.tag}`, `${message.author.displayAvatarURL({ format: 'png', dynamic: true })}`)
+        message.channel.send(userInfo)
+    },
+}
