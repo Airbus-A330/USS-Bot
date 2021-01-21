6136cb4bcf5c41e71ec15a3f9f7f008beed142e1
AiyrTech <danielrperkins05@gmail.com>
2021-01-21 13:45:10 +0000

    stuff

--- src/commands/utility/userinfo.js
+++ src/commands/utility/userinfo.js
@@ -5,11 +5,12 @@ module.exports = {
     description: 'get info on  user',
     execute (client, message, args) {
         let user = message.mentions.users.first();
+        message.channel.send(user);
         if (!user) {
             if (!Number.isNaN(args[0])) {
-                user = message.guild.users.cache.get(args[0]).tag;
+                user = message.guild.users.cache.get(args[0]);
+                message.channel.send(user);
             }
         }
-        console.log(user);
     }
 }
