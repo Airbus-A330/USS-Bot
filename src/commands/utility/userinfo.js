1160b41d04204501ae2d4f7799e408b16743f48c
AiyrTech <danielrperkins05@gmail.com>
2021-01-21 13:35:47 +0000

    stuff

---
+++ src/commands/utility/userinfo.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'userinfo',
+    description: 'get info on  user',
+    execute (client, message, args) {
+        let user = message.mentions.users.first();
+        if (!user) {
+            if (!Number.isNaN(args[0])) {
+                user = message.guild.users.cache.get(`${args[0]}`).tag;
+            }
+        }
+        console.log(user);
+    }
+}
